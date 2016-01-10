c3f69217d587a52f23cf16e391be54eb290904c6
Zhang Bin <zncoder@gmail.com>
2016-01-10 14:23:52 -0800

    gosym implementation

---
+++ README.md
@@ -0,0 +1,28 @@
+# gosym
+
+Similar to godef, gosym prints the location of the definition of the symbol in
+the Go source code.
+
+It is based on `go/types` and `golang.org/x/tools/go/loader`, so it does not
+have the known limitations of godef. It uses `go/build` to find Go source code
+files with the correct `GOOS` and `GOARCH`.
+
+## Usage
+
+The minimal go version is go1.5, since gosym uses the `go/types` in the standard
+library.
+
+To build,
+
+```go get github.com/zncoder/gosym```
+
+For example, to find the definition of the symbol at position `100` in the file `foo.go`,
+
+```gosym -f foo.go -o 100``
+
+You can replace godef with gosym by symbolic linking gosym to godef. This should
+work with go-mode.el in Emacs.
+
+## TODO
+
+Gosym is slower than godef because of the use of loader.
---
+++ main.go
@@ -0,0 +1,297 @@
+// +build go1.5
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/build"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/tools/go/ast/astutil"
+	"golang.org/x/tools/go/loader"
+)
+
+var (
+	verbose  = flag.Bool("debug", false, "verbose")
+	filename = flag.String("f", "", "file")
+	offset   = flag.Int("o", 0, "offset in file, 0 based")
+	stdin    = flag.Bool("i", false, "read file from stdin")
+	// 2-pass is the fastest and loader is slowest
+	pass = flag.Int("p", 3, "parse algorithm. can be 1, 2 or 3. 2 is the fastest. 3 is slowest but most reliable")
+)
+
+func lg(format string, arg ...interface{}) {
+	if *verbose {
+		log.Printf(format, arg...)
+	}
+}
+
+func parseFile(fn string, src interface{}) *ast.File {
+	f, err := parser.ParseFile(fset, fn, src, parser.AllErrors)
+	if err != nil {
+		// error is expected
+		lg("parse file=%s err=%v", fn, err)
+	}
+	return f
+}
+
+func pkgPath(fn string) string {
+	dir := filepath.Dir(fn)
+	i := strings.LastIndex(dir, "/src/")
+	if i < 0 {
+		return "main"
+	}
+	return dir[i+len("/src/"):]
+}
+
+func tokenFile(f *ast.File) *token.File {
+	return fset.File(f.Package)
+}
+
+func printPos(pos token.Pos) string {
+	return fset.Position(pos).String()
+}
+
+func pkgFiles(p string) []string {
+	pkg, err := build.Import(p, "", 0)
+	if err != nil {
+		lg("import pkg=%s err=%v", p, err)
+		return nil
+	}
+	out := make([]string, len(pkg.GoFiles))
+	for i, f := range pkg.GoFiles {
+		out[i] = filepath.Join(pkg.Dir, f)
+	}
+	return out
+}
+
+func parseMyPkg() (myPkg string, fs []*ast.File, chain []ast.Node) {
+	myPkg = pkgPath(*filename)
+	fns := pkgFiles(myPkg)
+	if fns == nil {
+		fns = []string{*filename}
+	}
+	lg("files in pkg=%s: %v", myPkg, fns)
+
+	for _, fn := range fns {
+		var f *ast.File
+		if fn == *filename && *stdin {
+			b, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				log.Fatalf("read stdin err=%v", err)
+			}
+			f = parseFile(fn, b)
+		} else {
+			f = parseFile(fn, nil)
+		}
+
+		fs = append(fs, f)
+
+		if fn == *filename {
+			tf := tokenFile(f)
+			pos := tf.Pos(*offset)
+			if pos == token.NoPos {
+				log.Fatalf("no pos found")
+			}
+			lg("pos=%v", printPos(pos))
+			chain, _ = astutil.PathEnclosingInterval(f, pos, pos+1)
+		}
+	}
+	return myPkg, fs, chain
+}
+
+func findIdent(chain []ast.Node) *ast.Ident {
+	for _, nd := range chain {
+		if id, ok := nd.(*ast.Ident); ok {
+			return id
+		}
+	}
+	return nil
+}
+
+type srcImporter struct {
+	cfg types.Config
+}
+
+func newSrcImporter() srcImporter {
+	return srcImporter{
+		cfg: types.Config{
+			Importer: importer.Default(),
+			Error:    func(err error) {},
+			DisableUnusedImportCheck: true,
+		},
+	}
+}
+
+func importSrcPkg(cfg *types.Config, path string) (*types.Package, error) {
+	fns := pkgFiles(path)
+	var fs []*ast.File
+	for _, fn := range fns {
+		f := parseFile(fn, nil)
+		fs = append(fs, f)
+	}
+
+	return cfg.Check(path, fset, fs, nil)
+}
+
+func (si srcImporter) Import(path string) (*types.Package, error) {
+	return importSrcPkg(&si.cfg, path)
+}
+
+type hybridImporter struct {
+	cfg      types.Config
+	pkgInUse string
+}
+
+func newHybridImporter(pkg string) *hybridImporter {
+	return &hybridImporter{
+		pkgInUse: pkg,
+		cfg: types.Config{
+			Importer: importer.Default(),
+			Error:    func(err error) {},
+			DisableUnusedImportCheck: true,
+		},
+	}
+}
+
+func (hi *hybridImporter) Import(path string) (*types.Package, error) {
+	if path != hi.pkgInUse {
+		// TODO: try cfg when the default importer fails to find the binary package.
+		lg("import pkg=%s using default importer", path)
+		return hi.cfg.Importer.Import(path)
+	}
+	return importSrcPkg(&hi.cfg, hi.pkgInUse)
+}
+
+var fset = token.NewFileSet()
+
+func main() {
+	// flags used by godef
+	flag.Bool("A", false, "")
+	flag.Bool("a", false, "")
+	flag.Bool("acme", false, "")
+	flag.Bool("t", false, "")
+	flag.Parse()
+	*filename, _ = filepath.Abs(*filename)
+	lg("args=%v", os.Args)
+
+	myPkg, fs, chain := parseMyPkg()
+	target := findIdent(chain)
+	lg("target is %v@%s", target, printPos(target.Pos()))
+
+	switch *pass {
+	case 1:
+		onePass(myPkg, fs, target)
+	case 2:
+		twoPass(myPkg, fs, target)
+	case 3:
+		parseProgram(myPkg, fs, target)
+	}
+}
+
+func onePass(myPkg string, fs []*ast.File, target *ast.Ident) {
+	cfg := types.Config{
+		Importer: newSrcImporter(),
+		Error:    func(err error) {},
+		DisableUnusedImportCheck: true,
+	}
+	info := types.Info{
+		Uses: make(map[*ast.Ident]types.Object),
+	}
+	cfg.Check(myPkg, fset, fs, &info)
+	obj := info.Uses[target]
+	lg("target=%v uses obj=%v", target, obj)
+	printTargetObj(obj)
+}
+
+func printTargetObj(obj types.Object) {
+	if obj != nil && obj.Pos() != token.NoPos {
+		fmt.Println(printPos(obj.Pos()))
+	}
+}
+
+func twoPass(myPkg string, fs []*ast.File, target *ast.Ident) {
+	// first pass to find out the package of target
+	cfg := types.Config{
+		Importer: newHybridImporter(""),
+		Error:    func(err error) {},
+		DisableUnusedImportCheck: true,
+	}
+	info := types.Info{
+		Uses: make(map[*ast.Ident]types.Object),
+	}
+	cfg.Check(myPkg, fset, fs, &info)
+
+	obj := info.Uses[target]
+	if obj == nil {
+		log.Fatalf("object of target=%v not found", target)
+	}
+	// BUG:https://github.com/golang/go/issues/13898
+	otherPkg := obj.Pkg().Path()
+	lg("obj of target=%v is %v in pkg=%s", target, obj, otherPkg)
+
+	if otherPkg == myPkg {
+		lg("found in mypkg")
+		printTargetObj(obj)
+		return
+	}
+
+	// second pass to find out the object of target
+	cfg.Importer = newHybridImporter(otherPkg)
+	info.Uses = make(map[*ast.Ident]types.Object)
+	cfg.Check(myPkg, fset, fs, &info)
+	obj = info.Uses[target]
+	lg("target=%v in otherpkg obj=%v", target, obj)
+	printTargetObj(obj)
+}
+
+func parseProgram(myPkg string, fs []*ast.File, target *ast.Ident) {
+	cfg := loader.Config{
+		Fset:       fset,
+		ParserMode: parser.AllErrors,
+		TypeChecker: types.Config{
+			Error: func(error) {},
+			DisableUnusedImportCheck: true,
+		},
+		TypeCheckFuncBodies: func(path string) bool {
+			return path == myPkg
+		},
+		AllowErrors: true,
+	}
+	os.Setenv("CGO_ENABLED", "0")
+
+	cfg.CreateFromFiles(myPkg, fs...)
+	prog, err := cfg.Load()
+	if err != nil {
+		lg("load program err=%v", err)
+	}
+
+	obj := findIdentObj(prog, target)
+	lg("target=%v uses obj=%v", target, obj)
+	printTargetObj(obj)
+}
+
+func findIdentObj(prog *loader.Program, target *ast.Ident) types.Object {
+	pkg := prog.Created[0]
+	for id, obj := range pkg.Defs {
+		if target == id {
+			return obj
+		}
+	}
+	for id, obj := range pkg.Uses {
+		if target == id {
+			return obj
+		}
+	}
+	return nil
+}
