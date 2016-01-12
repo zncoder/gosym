ab607f9dbc091c964afc442fe3de49995afcd987
Zhang Bin <zncoder@gmail.com>
2016-01-11 22:43:56 -0800

    - make it work with test files - make failure compatible with godef - fix the off-by-one bug of the offset

--- main.go
+++ main.go
@@ -22,16 +22,21 @@ import (
 )
 
 var (
-	verbose  = flag.Bool("debug", false, "verbose")
-	filename = flag.String("f", "", "file")
-	offset   = flag.Int("o", 0, "offset in file, 0 based")
-	stdin    = flag.Bool("i", false, "read file from stdin")
+	verbose   = flag.Bool("debug", false, "verbose")
+	logToFile = flag.Bool("log", false, "log to tmp file")
+	filename  = flag.String("f", "", "file")
+	offset    = flag.Int("o", 0, "offset in file, 1 based")
+	stdin     = flag.Bool("i", false, "read file from stdin")
 	// 2-pass is the fastest and loader is slowest
 	pass = flag.Int("p", 3, "parse algorithm. can be 1, 2 or 3. 2 is the fastest. 3 is slowest but most reliable")
 )
 
+var logf *os.File
+
 func lg(format string, arg ...interface{}) {
-	if *verbose {
+	if logf != nil {
+		fmt.Fprintf(logf, format+"\n", arg...)
+	} else if *verbose {
 		log.Printf(format, arg...)
 	}
 }
@@ -68,11 +73,32 @@ func pkgFiles(p string) []string {
 		lg("import pkg=%s err=%v", p, err)
 		return nil
 	}
-	out := make([]string, len(pkg.GoFiles))
+
+	// TODO: use Imports in one-pass to parse fewer files.
+	lg("pkg=%s imports %v", p, pkg.Imports)
+
+	isTest := isTestFile(*filename)
+	n := len(pkg.GoFiles)
+	m := n
+	if isTest {
+		n += len(pkg.TestGoFiles)
+	}
+
+	out := make([]string, n)
 	for i, f := range pkg.GoFiles {
 		out[i] = filepath.Join(pkg.Dir, f)
 	}
+	if isTest {
+		for i, f := range pkg.TestGoFiles {
+			out[m+i] = filepath.Join(pkg.Dir, f)
+		}
+	}
 	return out
+}
+
+func isTestFile(fn string) bool {
+	fn = strings.TrimSuffix(fn, filepath.Ext(fn))
+	return strings.HasSuffix(fn, "_test")
 }
 
 func parseMyPkg() (myPkg string, fs []*ast.File, chain []ast.Node) {
@@ -101,7 +127,8 @@ func parseMyPkg() (myPkg string, fs []*ast.File, chain []ast.Node) {
 			tf := tokenFile(f)
 			pos := tf.Pos(*offset)
 			if pos == token.NoPos {
-				log.Fatalf("no pos found")
+				lg("no pos found")
+				fail()
 			}
 			lg("pos=%v", printPos(pos))
 			chain, _ = astutil.PathEnclosingInterval(f, pos, pos+1)
@@ -173,32 +200,6 @@ func (hi *hybridImporter) Import(path string) (*types.Package, error) {
 	return importSrcPkg(&hi.cfg, hi.pkgInUse)
 }
 
-var fset = token.NewFileSet()
-
-func main() {
-	// flags used by godef
-	flag.Bool("A", false, "")
-	flag.Bool("a", false, "")
-	flag.Bool("acme", false, "")
-	flag.Bool("t", false, "")
-	flag.Parse()
-	*filename, _ = filepath.Abs(*filename)
-	lg("args=%v", os.Args)
-
-	myPkg, fs, chain := parseMyPkg()
-	target := findIdent(chain)
-	lg("target is %v@%s", target, printPos(target.Pos()))
-
-	switch *pass {
-	case 1:
-		onePass(myPkg, fs, target)
-	case 2:
-		twoPass(myPkg, fs, target)
-	case 3:
-		parseProgram(myPkg, fs, target)
-	}
-}
-
 func onePass(myPkg string, fs []*ast.File, target *ast.Ident) {
 	cfg := types.Config{
 		Importer: newSrcImporter(),
@@ -214,9 +215,17 @@ func onePass(myPkg string, fs []*ast.File, target *ast.Ident) {
 	printTargetObj(obj)
 }
 
+// compatible with godef
+func fail() {
+	fmt.Fprintln(os.Stderr, "godef: no identifier found")
+	os.Exit(2)
+}
+
 func printTargetObj(obj types.Object) {
 	if obj != nil && obj.Pos() != token.NoPos {
 		fmt.Println(printPos(obj.Pos()))
+	} else {
+		fail()
 	}
 }
 
@@ -234,7 +243,8 @@ func twoPass(myPkg string, fs []*ast.File, target *ast.Ident) {
 
 	obj := info.Uses[target]
 	if obj == nil {
-		log.Fatalf("object of target=%v not found", target)
+		lg("object of target=%v not found", target)
+		fail()
 	}
 	// BUG:https://github.com/golang/go/issues/13898
 	otherPkg := obj.Pkg().Path()
@@ -294,4 +304,45 @@ func findIdentObj(prog *loader.Program, target *ast.Ident) types.Object {
 		}
 	}
 	return nil
+}
+
+var fset = token.NewFileSet()
+
+func main() {
+	// flags used by godef
+	flag.Bool("A", false, "")
+	flag.Bool("a", false, "")
+	flag.Bool("acme", false, "")
+	flag.Bool("t", false, "")
+	flag.Parse()
+
+	if *logToFile {
+		logf, _ = ioutil.TempFile(os.TempDir(), "gosym-log.")
+	}
+
+	*filename, _ = filepath.Abs(*filename)
+
+	// offset is 1-based, but token.File.Offset is 0-based.
+	*offset--
+	if *offset < 0 {
+		fail()
+	}
+
+	lg("args=%v", os.Args)
+
+	myPkg, fs, chain := parseMyPkg()
+	target := findIdent(chain)
+	if target == nil {
+		fail()
+	}
+	lg("target is %v@%v", target, printPos(target.Pos()))
+
+	switch *pass {
+	case 1:
+		onePass(myPkg, fs, target)
+	case 2:
+		twoPass(myPkg, fs, target)
+	case 3:
+		parseProgram(myPkg, fs, target)
+	}
 }
